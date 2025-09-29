0bf1af7ed5a66cd09a1678168b77ced51af098cf
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-09-30 03:53:42 +0800

    draft 2

--- PrjF1G8Item.java
+++ PrjF1G8Item.java
@@ -1,14 +1,5 @@
 class Item {
-    String name;
-    int size;
-    
-    public Item(String name, int size){
-        this.name = name;
-        this.size = size;
-    }
+        //code here
+
 }
 
-/*
-Item class - This class has attributes for the item, a constructor that can initialize item and toString
-method that returns the information needed to view the item information.
- */
--- PrjF1G8Main.java
+++ PrjF1G8Main.java
@@ -1,12 +1,5 @@
 public class PrjF1G8Main {
     public static void main(String[] args) {
-    Item i1 = new Item("Book", 3);
-    System.out.println(i1.name + " " + i1.size);
+    //code here
     }
 }
-
-/*
- Main class – This class instantiates an array of storage box objects. It provides users with a menu
-for the 3 main activities. It allows the users to repeat the activities until they choose to exit the
-program. It also includes simple input validation.
- */
