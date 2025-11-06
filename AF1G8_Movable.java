5b357fd4dbd893c0e1c26ecd323c60b9a26a1f13
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-11-06 20:21:08 +0800

    assignment arrangement

---
+++ AF1G8_Movable.java
@@ -0,0 +1,9 @@
+public interface AF1G8_Movable {
+    void turnOn();
+    void turnOff();
+    void moveForward();
+    void moveBackward();
+    void stop();
+    void turnLeft();
+    void turnRight();
+}
---
+++ AF1G8_ToyCar.java
@@ -0,0 +1,22 @@
+public class AF1G8_ToyCar extends AF1G8_Car {
+    public void turnOn() {
+    }
+
+    public void turnOff() {
+    }
+
+    public void moveForward() {
+    }
+
+    public void moveBackward() {
+    }
+
+    public void stop() {
+    }
+
+    public void turnLeft() {
+    }
+
+    public void turnRight() {
+    }
+}
