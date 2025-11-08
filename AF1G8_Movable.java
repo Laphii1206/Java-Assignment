6cfe708b769fe108b754f581c13befba249d6e29
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-11-08 19:37:15 +0800

    Delete AF1G8_Movable.java

--- AF1G8_Movable.java
+++
@@ -1,9 +0,0 @@
-public interface AF1G8_Movable {
-    void turnOn();
-    void turnOff();
-    void moveForward();
-    void moveBackward();
-    void stop();
-    void turnLeft();
-    void turnRight();
-}
